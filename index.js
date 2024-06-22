168a36177d615ccd1d70804f41a6dccd303149dc
Kumaresh <kumareshappu12@gmail.com>
2024-06-22 16:11:22 +0530

    UPDATE CODE

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: 'http://127.0.0.1:5500', // Replace with your allowed domain
+    origin: '*',
     methods: ['POST'], // Allow only POST requests
   }),
 );
@@ -23,16 +23,16 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// API endpoint to send email
 app.post('/send-email', (req, res) => {
-  const { name, email, message } = req.body;
+  const { name, email, subject, phone, message } = req.body;
 
   const mailOptions = {
     from: fromEmail,
     to: 'kumaresh12012@gmail.com',
-    subject: 'New Contact Form Submission',
-    // text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+    // to: 'petindiaindustries.com',
+    subject: subject || 'New Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`, // Plain text body
+    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Phone: ${phone}</p><p>Message: ${message}</p>`, // HTML body
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
