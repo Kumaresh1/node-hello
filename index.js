449ffb5157cf388a968045b25107051ca3ff9b8b
Kumaresh <kumareshappu12@gmail.com>
2024-06-23 12:39:57 +0530

    add cors

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: '*',
-    methods: ['POST'], // Allow only POST requests
-  }),
-);
+const allowedOrigins = [
+  'https://petindiaindustries.com', // Add more origins if needed
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['POST'], // Allow only POST requests
+};
+
+app.use(cors(corsOptions));
 
 const fromEmail = 'alerts.toeatz@gmail.com';
 const transporter = nodemailer.createTransport({
@@ -43,6 +53,16 @@ app.post('/send-email', (req, res) => {
     }
     res.send({ success: true, message: 'Email sent successfully', info });
   });
+
+  // mailOptions.to = 'petindiaindustries.com';
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //     return res
+  //       .status(500)
+  //       .send({ success: false, message: 'Failed to send email', error });
+  //   }
+  //   res.send({ success: true, message: 'Email sent successfully', info });
+  // });
 });
 
 app.listen(port, () => {
